2f0eaa3cb283f65a28e737221cf8c8ce3c6def27
badhex <badhex@inflict.io>
2023-08-18 11:06:43 -0500

    Added a variable for the log file and README.md.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Mirror Key
+A python script to monitor the POE log file and press a defined set of keys when detecting a character death.
+
+## Defaults
+* The program assumes your capture hotkey is CTRL_r + SHIFT_r + F10.
+  * You can simply set your capture hotkey to this combo
+  * OR You can modify this in the helpers keysender.py file
+
+## Setup
+* Edit the log_file variable to point to the correct POE log file. An example location is set by default.
--- src/mirrorkey.py
+++ src/mirrorkey.py
@@ -3,6 +3,7 @@ from threading import Thread
 from helpers.logwatcher import LogWatcher
 from helpers.keysender import KeySender
 
+log_file = """E:\\Games\\Grinding Gear Games\\Path of Exile\\logs\\Client.txt"""
 debug = False
 game_name = "Path of Exile"
 key_sender = None
@@ -16,7 +17,7 @@ def send_hotkey(line):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     log_watcher = LogWatcher(
-        log_file="""E:\\Games\\Grinding Gear Games\\Path of Exile\\logs\\Client.txt""",
+        log_file=log_file,
         test=debug,
         regex_log=re.compile("""^([\d\/]*) ([\d:]*) ([\da-f]*) ([\da-f]*) \[(.*)\] (.*)$"""),
         slain_regex=re.compile("""^(.*) has been slain\."""),
